885a0105b3e1eee34eec6bb61e13b017795bc9af
Ruslan <rusmuhamed@gmail.com>
2018-11-26 16:04:24 +0300

    bug fix

--- resources/views/admin/expert/index.blade.php
+++ resources/views/admin/expert/index.blade.php
@@ -5,72 +5,75 @@
         <div class="row justify-content-center">
             <div class="col">
                 <h1>Члены жюри</h1>
-                <table class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th width="50">ID</th>
-                        <th>ФИО</th>
-                        <th>Ссылка</th>
-                        <th width="180">Жюри или эксперт</th>
-                        <th class="text-center" width="80">
-                            <a class="btn btn-sm btn-success" href="{{ route('admin.expert.create') }}"
-                               role="button"><span class="oi oi-plus" title="Create expert"
-                                                   aria-hidden="true"></span></a>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @forelse($experts as $expert)
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <thead>
                         <tr>
-                            <td>{{ $expert->id }}</td>
-                            <td>{{ $expert->name }}</td>
-                            <td>{{ route('signin.byToken', $expert->token) }}</td>
-                            <td>{{ $expert->expert_type == 0 ? "Жюри" : "Эксперт" }}</td>
-                            <td class="justify-content-center">
-                                <div class="btn-group btn-group-sm">
-                                    <a href="{{ route('admin.expert.edit', ['id' => $expert->id]) }}"
-                                       class="btn btn-warning" role="button">
-                                        <span class="oi oi-pencil" title="Edit expert" aria-hidden="true"></span>
-                                    </a>
-                                    <a class="for-modal btn btn-danger" data-toggle="modal" role="button" href="#"
-                                       data-target="#expertModal{{$expert->id}}">
-                                        <span class="oi oi-trash" title="Delete expert" aria-hidden="true"></span>
-                                    </a>
-                                </div>
-                                <div class="modal fade" id="expertModal{{$expert->id}}" tabindex="-1" role="dialog"
-                                     aria-labelledby="expertModal{{$expert->id}}Label" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="expertModal{{$expert->id}}Label">
-                                                    Удаление</h5>
-                                                <button type="button" class="close" data-dismiss="modal"
-                                                        aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p>Удалить члена жюри "{{ $expert->name }}"?</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                                    Отмена
-                                                </button>
-                                                <a href="{{ route('admin.expert.delete', ['id' => $expert->id]) }}"
-                                                   class="btn btn-primary">Удалить</a>
+                            <th width="50">ID</th>
+                            <th>ФИО</th>
+                            <th>Ссылка</th>
+                            <th width="180">Жюри или эксперт</th>
+                            <th class="text-center" width="80">
+                                <a class="btn btn-sm btn-success" href="{{ route('admin.expert.create') }}"
+                                   role="button"><span class="oi oi-plus" title="Create expert"
+                                                       aria-hidden="true"></span></a>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse($experts as $expert)
+                            <tr>
+                                <td>{{ $expert->id }}</td>
+                                <td>{{ $expert->name }}</td>
+                                <td>{{ route('signin.byToken', $expert->token) }}</td>
+                                <td>{{ $expert->expert_type == 0 ? "Жюри" : "Эксперт" }}</td>
+                                <td class="justify-content-center">
+                                    <div class="btn-group btn-group-sm">
+                                        <a href="{{ route('admin.expert.edit', ['id' => $expert->id]) }}"
+                                           class="btn btn-warning" role="button">
+                                            <span class="oi oi-pencil" title="Edit expert" aria-hidden="true"></span>
+                                        </a>
+                                        <a class="for-modal btn btn-danger" data-toggle="modal" role="button" href="#"
+                                           data-target="#expertModal{{$expert->id}}">
+                                            <span class="oi oi-trash" title="Delete expert" aria-hidden="true"></span>
+                                        </a>
+                                    </div>
+                                    <div class="modal fade" id="expertModal{{$expert->id}}" tabindex="-1" role="dialog"
+                                         aria-labelledby="expertModal{{$expert->id}}Label" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="expertModal{{$expert->id}}Label">
+                                                        Удаление</h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>Удалить члена жюри "{{ $expert->name }}"?</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                            data-dismiss="modal">
+                                                        Отмена
+                                                    </button>
+                                                    <a href="{{ route('admin.expert.delete', ['id' => $expert->id]) }}"
+                                                       class="btn btn-primary">Удалить</a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="text-center">Не создано ни одного члена жюри</td>
-                        </tr>
-                    @endforelse
-                    </tbody>
-                </table>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="5" class="text-center">Не создано ни одного члена жюри</td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -5,88 +5,92 @@
         <div class="row justify-content-center">
             <div class="col">
                 <h1>Доклады</h1>
-                <table class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th width="50">ID</th>
-                        <th>Название</th>
-                        <th>ФИО докладчика</th>
-                        <th>Должность</th>
-                        <th>Филиал</th>
-                        <th>С</th>
-                        <th>До</th>
-                        <th class="text-center" width="80">
-                            <a class="btn btn-sm btn-success" href="{{ route('admin.report.create') }}"
-                               role="button"><span class="oi oi-plus" title="Create report"
-                                                   aria-hidden="true"></span></a>
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @forelse($reports as $report)
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <thead>
                         <tr>
-                            <td>{{ $report->id }}</td>
-                            <td>{{ $report->name }}</td>
-                            <td>{{ $report->reporter }}</td>
-                            <td>{{ $report->position }}</td>
-                            <td>{{ $report->filial }}</td>
-                            <td>{{ $report->from }}</td>
-                            <td>{{ $report->to }}</td>
-                            <td class="justify-content-center">
-                                <div class="btn-group btn-group-sm">
-                                    <a href="{{ route('admin.report.show', ['id' => $report->id]) }}"
-                                       class="btn btn-success" role="button" target="_blank">
-                                        <span class="oi oi-eye" title="Show report" aria-hidden="true"></span>
-                                    </a>
-                                    <a href="{{ route('admin.report.toggle', ['id' => $report->id]) }}"
-                                       class="btn {{ $report->active ? 'btn-success' : 'btn-danger' }}" role="button">
+                            <th width="50">ID</th>
+                            <th>Название</th>
+                            <th>ФИО докладчика</th>
+                            <th>Должность</th>
+                            <th>Филиал</th>
+                            <th>С</th>
+                            <th>До</th>
+                            <th class="text-center" width="80">
+                                <a class="btn btn-sm btn-success" href="{{ route('admin.report.create') }}"
+                                   role="button"><span class="oi oi-plus" title="Create report"
+                                                       aria-hidden="true"></span></a>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse($reports as $report)
+                            <tr>
+                                <td>{{ $report->id }}</td>
+                                <td>{{ $report->name }}</td>
+                                <td>{{ $report->reporter }}</td>
+                                <td>{{ $report->position }}</td>
+                                <td>{{ $report->filial }}</td>
+                                <td>{{ $report->from }}</td>
+                                <td>{{ $report->to }}</td>
+                                <td class="justify-content-center">
+                                    <div class="btn-group btn-group-sm">
+                                        <a href="{{ route('admin.report.show', ['id' => $report->id]) }}"
+                                           class="btn btn-success" role="button" target="_blank">
+                                            <span class="oi oi-eye" title="Show report" aria-hidden="true"></span>
+                                        </a>
+                                        <a href="{{ route('admin.report.toggle', ['id' => $report->id]) }}"
+                                           class="btn {{ $report->active ? 'btn-success' : 'btn-danger' }}"
+                                           role="button">
                                         <span class="oi {{ $report->active ? 'oi-check' : 'oi-x' }}"
                                               title="{{ $report->active ? 'Disable' : 'Enable' }} report"
                                               aria-hidden="true"></span>
-                                    </a>
-                                    <a href="{{ route('admin.report.edit', ['id' => $report->id]) }}"
-                                       class="btn btn-warning" role="button">
-                                        <span class="oi oi-pencil" title="Edit report" aria-hidden="true"></span>
-                                    </a>
-                                    <a class="for-modal btn btn-danger" data-toggle="modal" role="button" href="#"
-                                       data-target="#reportModal{{$report->id}}">
-                                        <span class="oi oi-trash" title="Delete report" aria-hidden="true"></span>
-                                    </a>
-                                </div>
-                                <div class="modal fade" id="reportModal{{$report->id}}" tabindex="-1" role="dialog"
-                                     aria-labelledby="reportModal{{$report->id}}Label" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="reportModal{{$report->id}}Label">
-                                                    Удаление</h5>
-                                                <button type="button" class="close" data-dismiss="modal"
-                                                        aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <p>Удалить доклад "{{ $report->name }}"?</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                                    Отмена
-                                                </button>
-                                                <a href="{{ route('admin.report.delete', ['id' => $report->id]) }}"
-                                                   class="btn btn-primary">Удалить</a>
+                                        </a>
+                                        <a href="{{ route('admin.report.edit', ['id' => $report->id]) }}"
+                                           class="btn btn-warning" role="button">
+                                            <span class="oi oi-pencil" title="Edit report" aria-hidden="true"></span>
+                                        </a>
+                                        <a class="for-modal btn btn-danger" data-toggle="modal" role="button" href="#"
+                                           data-target="#reportModal{{$report->id}}">
+                                            <span class="oi oi-trash" title="Delete report" aria-hidden="true"></span>
+                                        </a>
+                                    </div>
+                                    <div class="modal fade" id="reportModal{{$report->id}}" tabindex="-1" role="dialog"
+                                         aria-labelledby="reportModal{{$report->id}}Label" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="reportModal{{$report->id}}Label">
+                                                        Удаление</h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <p>Удалить доклад "{{ $report->name }}"?</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                            data-dismiss="modal">
+                                                        Отмена
+                                                    </button>
+                                                    <a href="{{ route('admin.report.delete', ['id' => $report->id]) }}"
+                                                       class="btn btn-primary">Удалить</a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="8" class="text-center">Не создано ни одного доклада</td>
-                        </tr>
-                    @endforelse
-                    </tbody>
-                </table>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="8" class="text-center">Не создано ни одного доклада</td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
