6d97180bd64275c2c076076be705b83fdfbfb0a4
medved_v_ushanke <getingjagare@gmail.com>
2019-05-26 20:39:59 +0300

    routes fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if ($request->getRequestUri() == "/") {
+        if (in_array($request->getRequestUri(), ['/', 'vote'])) {
             return route('signin');
         }
 
